12000b7981923b6e0b1ea4074d9ba1f9ef64302e
Mikael Jergefelt <mikael.jergefelt@ki.se>
2015-08-09 22:08:47 +0200

    add basic RFduino implementation

---
+++ eddystone-url/implementations/RFduino/eddystone-url.ino
@@ -0,0 +1,29 @@
+#include <RFduinoBLE.h>
+
+uint8_t advdata[] =
+{
+  0x03,  // Length
+  0x03,  // Param: Service List
+  0xAA, 0xFE,  // Eddystone ID
+  0x0A,  // Length
+  0x16,  // Service Data
+  0xAA, 0xFE, // Eddystone ID
+  0x10,  // URL flag
+  0xEB,  // Power
+  0x02,  // http://
+  'a',
+  'b',
+  'c',
+  0x07,  // .com
+};
+
+void setup() {
+  RFduinoBLE_advdata = advdata;
+  RFduinoBLE_advdata_len = sizeof(advdata);
+  RFduinoBLE.advertisementInterval = 700; // Advertise every 700ms
+  RFduinoBLE.begin();
+}
+
+void loop() {
+  RFduino_ULPDelay(INFINITE);   // Switch to lower power mode
+}
